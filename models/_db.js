da3b9bc3420ccfd2aaa89f5763e26077c23cc135
Elaine <ljc391@nyu.edu>
2017-04-26 15:58:17 -0400

    heroku database

--- models/_db.js
+++ models/_db.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize');
 
-var db = new Sequelize('postgres://localhost:5432/food', {
+//postgres://jyrnheaglohdce:f0bc8ca435047a1314b79e6379faa6851c0039de35175842daa59fb6db6f4c87@ec2-54-163-254-76.compute-1.amazonaws.com:5432/d98mh062mviuhb
+//postgres://localhost:5432/food
+var db = new Sequelize('postgres://jyrnheaglohdce:f0bc8ca435047a1314b79e6379faa6851c0039de35175842daa59fb6db6f4c87@ec2-54-163-254-76.compute-1.amazonaws.com:5432/d98mh062mviuhb', {
   logging: false
 });
 
