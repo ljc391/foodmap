d705f6cc004aa0972fb97dcf61dde016180e4a0d
Elaine <ljc391@nyu.edu>
2017-05-01 17:00:57 -0400

    media query

--- models/_db.js
+++ models/_db.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize');
 //postgres://jyrnheaglohdce:f0bc8ca435047a1314b79e6379faa6851c0039de35175842daa59fb6db6f4c87@ec2-54-163-254-76.compute-1.amazonaws.com:5432/d98mh062mviuhb
 //postgres://localhost:5432/food
 var url = 'postgres://jyrnheaglohdce:f0bc8ca435047a1314b79e6379faa6851c0039de35175842daa59fb6db6f4c87@ec2-54-163-254-76.compute-1.amazonaws.com:5432/d98mh062mviuhb';
-var db = new Sequelize('postgres://localhost:5432/food', {
+var db = new Sequelize(url, {
   logging: false
 });
 
